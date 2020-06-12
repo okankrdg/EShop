f45c31adb6e4d8799bc2bb00eaf55de2538bccae
okancan karadağ <karadagokancan@gmail.com>
2020-06-12 20:24:16 +0300

    DbContext  && EntityConfig

---
+++ src/Infrastructre/Data/Config/BrandConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructre.Data.Config
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+            builder.Property(x => x.BrandName)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
