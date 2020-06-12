549f1014f69ff53dd7f65605b3d47320f3b62283
okancan karadağ <karadagokancan@gmail.com>
2020-06-12 18:40:21 +0300

    Entities & ShopContext

---
+++ src/Infrastructre/Data/ShopContext.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Entities;
+using Infrastructre.Data.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Infrastructre.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+         public DbSet<Category> Categories { get; set; }
+         public DbSet<Brand> Brands { get; set; }
+         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // https://www.learnentityframeworkcore.com/configuration/fluent-api
+            //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
