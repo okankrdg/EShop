8a48eac7fc3a54480d99c7936927273844e26b3b
okancan karadağ <karadagokancan@gmail.com>
2020-06-15 18:53:26 +0300

    Seed

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructre.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +13,24 @@ namespace Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host=CreateHostBuilder(args).Build();
+            using(var scope= host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    var shopContext = services.GetRequiredService<ShopContext>();
+                    await ShopContextSeed.SeedAsync(shopContext);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occured seeding th database");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
